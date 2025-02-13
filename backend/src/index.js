567a55786b39d3e1c16cb1f2bc867e916412f244
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-02-13 16:32:37 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ const port = process.env.PORT || 5001;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
   });
 }
 server.listen(port, () => {
