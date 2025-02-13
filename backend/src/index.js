895fbf5d5745003f10c16843cbcb93fc3b1a8c2e
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-02-13 16:30:47 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
-import express from "express";
 import path from "path";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -7,7 +7,6 @@ import connectDB from "./lib/db.js";
 import authRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-
 dotenv.config();
 const __dirname = path.resolve();
 
@@ -17,26 +16,21 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
-// Serve Frontend in Production
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.resolve("frontend", "build");
-  app.use(express.static(frontendPath));
+const port = process.env.PORT || 5001;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   });
 }
-
-const port = process.env.PORT || 5001;
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
   connectDB();
 });
